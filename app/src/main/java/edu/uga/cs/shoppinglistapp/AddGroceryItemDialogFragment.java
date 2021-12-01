9c3e8c8c8c7c2e17fa01ee994209974310e51158
Aviel Sabbag <76707692+AvielSabbag@users.noreply.github.com>
2021-11-30 19:59:25 -0500

    Worked on implementing view Items in list user Story
    Need to do:
    get add item to work and finish view items in list

---
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/AddGroceryItemDialogFragment.java
@@ -0,0 +1,95 @@
+package edu.uga.cs.shoppinglistapp;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AddGroceryItemDialogFragment extends DialogFragment {
+    private EditText itemName;
+    private EditText itemDescription;
+    private String userSubmitted;
+
+    public interface AddGroceryItemDialogListener {
+        void onFinishNewGroceryItemDialog(GroceryItem groceryItem);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Create the AlertDialog view
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View layout = inflater.inflate(R.layout.add_grocery_item_dialog,
+                (ViewGroup) getActivity().findViewById(R.id.root));
+
+        // get the view objects in the AlertDialog
+        itemName = layout.findViewById( R.id.editText1 );
+        itemDescription = layout.findViewById( R.id.editText4 );
+
+        // create a new AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AlertDialogStyle);
+        // Set its view (inflated above).
+        builder.setView(layout);
+
+        // Set the title of the AlertDialog
+        builder.setTitle( "New Grocery Item" );
+        // Provide the negative button listener
+        builder.setNegativeButton( android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // close the dialog
+                dialog.dismiss();
+            }
+        });
+        // Provide the positive button listener
+        builder.setPositiveButton( android.R.string.ok, new ButtonClickListener() );
+
+        // Create the AlertDialog and show it
+        return builder.create();
+    }
+
+    private class ButtonClickListener implements DialogInterface.OnClickListener {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            String itemNameString = itemName.getText().toString();
+            String itemDescriptionString = itemDescription.getText().toString();
+            FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                    if( currentUser != null ) {
+                        // User is signed in
+                        userSubmitted = currentUser.getEmail();
+                    } else {
+                        // User is signed out
+                        userSubmitted = "empty";
+                    }
+                }
+            });
+
+            GroceryItem groceryItem = new GroceryItem(itemNameString, itemDescriptionString);
+
+            // get the Activity's listener to add the new job lead
+            AddGroceryItemDialogListener listener = (AddGroceryItemDialogListener) ();
+            // add the new job lead
+            listener.onFinishNewGroceryItemDialog( groceryItem);
+            // close the dialog
+            dismiss();
+        }
+    }
+}
--- app/src/main/java/edu/uga/cs/shoppinglistapp/ShoppingListFragment.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/ShoppingListFragment.java
@@ -1,19 +1,43 @@
 package edu.uga.cs.shoppinglistapp;
 
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ShoppingListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ShoppingListFragment extends Fragment {
+public class ShoppingListFragment extends Fragment implements AddGroceryItemDialogFragment.AddGroceryItemDialogListener {
+
+    private List<GroceryItem> shoppingList;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter recyclerAdapter;
 
     public ShoppingListFragment() {
         // Required empty public constructor
@@ -38,6 +62,94 @@ public class ShoppingListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fullView = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+
+        Button newItem = fullView.findViewById(R.id.button5);
+        Button purchaseItem = fullView.findViewById(R.id.button6);
+
+        newItem.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new AddGroceryItemDialogFragment();
+                showDialogFragment(newFragment);
+            }
+        });
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("shoppingList");
+        recyclerView = (RecyclerView) fullView.findViewById(R.id.recyclerView);
+
+        shoppingList = new ArrayList<GroceryItem>();
+
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager( layoutManager );
+
+        myRef.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot snapshot ) {
+                // Once we have a DataSnapshot object, knowing that this is a list,
+                // we need to iterate over the elements and place them on a List.
+                for( DataSnapshot postSnapshot: snapshot.getChildren() ) {
+                    GroceryItem groceryItem = postSnapshot.getValue(GroceryItem.class);
+                    shoppingList.add(groceryItem);
+                }
+
+                // Now, create a JobLeadRecyclerAdapter to populate a ReceyclerView to display the job leads.
+                for(int i = 0; i< shoppingList.size(); i++) {
+                    Log.d("ShoppingListFragment", "onDataChange: Item #"+ i + ": " + shoppingList.get(i).toString());
+                }
+                recyclerAdapter = new GroceryRecyclerAdapter( shoppingList );
+                recyclerView.setAdapter( recyclerAdapter );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("ShoppingListFragment", "onCancelled: The read failed: " + databaseError.getMessage());
+            }
+        });
+
         return fullView;
     }
+
+    public void onFinishNewGroceryItemDialog(GroceryItem groceryItem) {
+        // add the new job lead
+        // Add a new element (JobLead) to the list of job leads in Firebase.
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("shoppingList");
+
+        // First, a call to push() appends a new node to the existing list (one is created
+        // if this is done for the first time).  Then, we set the value in the newly created
+        // list node to store the new job lead.
+        // This listener will be invoked asynchronously, as no need for an AsyncTask, as in
+        // the previous apps to maintain job leads.
+        myRef.push().setValue( groceryItem )
+                .addOnSuccessListener( new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        // Update the recycler view to include the new job lead
+                        shoppingList.add( groceryItem );
+                        recyclerAdapter.notifyItemInserted(shoppingList.size() - 1);
+
+                        Log.d( "ShoppingListFragment", "Grocery Item saved: " + groceryItem.toString() );
+                        // Show a quick confirmation
+                        Toast.makeText(getContext(), "Grocery Item added by: " + groceryItem.getUserSubmitted(),
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener( new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Toast.makeText( getContext(), "Failed to add grocery Item by: " + groceryItem.getUserSubmitted(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    void showDialogFragment( DialogFragment newFragment ) {
+        newFragment.show(getActivity().getSupportFragmentManager(), null);
+    }
+
+
+
 }
