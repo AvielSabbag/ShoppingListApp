c7b242df0b7eebab0c291a7efb59b21eef01a268
kevanjk <kevankad@gmail.com>
2021-12-05 19:54:25 -0500

    Added logout button action. Should be able to logout now.

--- app/src/main/java/edu/uga/cs/shoppinglistapp/ListViewerActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/ListViewerActivity.java
@@ -3,7 +3,10 @@ package edu.uga.cs.shoppinglistapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
@@ -17,6 +20,20 @@ public class ListViewerActivity extends AppCompatActivity {
         ViewPager2 viewPager = (ViewPager2) findViewById(R.id.viewPager);
         GroceryPagerAdapter groceryPagerAdapter = new GroceryPagerAdapter(this, this);
         viewPager.setAdapter(groceryPagerAdapter);
+        Button logoutButton = findViewById(R.id.logout);
 
+        logoutButton.setOnClickListener(new LogoutButtonClickListener());
     }
+
+    private class LogoutButtonClickListener implements View.OnClickListener{
+        @Override
+        public void onClick( View view ){
+            Intent intent = new Intent(view.getContext(), MainActivity.class);
+            view.getContext().startActivity(intent);
+        }
+    }
+
+
+
+
 }
